35b9422e023c5b7d4281f4d0874a394bbfa60cba
geanderson <esteves.gean@gmail.com>
2020-06-13 01:50:28 -0300

    add the run script

---
+++ src/run.sh
@@ -0,0 +1,6 @@
+python3 exploration.py ../experiments/ant.csv ant_f1.txt
+python3 exploration.py ../experiments/jedit.csv jedit_f1.txt
+python3 exploration.py ../experiments/log4j.csv log4j_f1.txt
+python3 exploration.py ../experiments/lucene.csv lucene_f1.txt
+python3 exploration.py ../experiments/xalan.csv xalan_f1.txt
+python3 exploration.py ../experiments/prop.csv prop_f1.txt
